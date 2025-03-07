6b3c5f4b0f35ceccd12c552a10448694bbd38823
HJKimdev04 <wva22lfr@kakao.com>
2025-03-08 00:34:50 +0900

    header css 수정 / footer 작성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from "./components/Header";
-
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
+        <Footer /> 
       </div>
     </Router>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+// src/components/Footer.js
+import React from 'react';
+import '../styles/Footer.css'; // 푸터 스타일링을 위한 CSS 파일
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 스레드 소개팅. 모든 권리 보유.</p>
+    </footer>
+  );
+};
+
+export default Footer;
